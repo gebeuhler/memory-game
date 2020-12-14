c14d154af4cee53c3ca13bfe939adbe5f57c6fda
George Beuhler <gebeuhler@gmail.com>
2020-12-13 20:57:00 -0600

    add images.

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,12 @@
             v-for="card in cards"
             :key="card.id"
             @click="flipCard(card)"
-            min-width="250"
+            min-height="150"
+            min-width="150"
             class="mr-4 mb-4"
           >
-            <v-card-text>
-              <div v-if="card.flipped">value: {{ card.value }}</div>
-              <div v-else>id: {{ card.id }}</div>
-            </v-card-text>
+            <v-img v-if="card.flipped" :src="card.src"></v-img>
+            {{ card.id }}
           </v-card>
         </div>
       </v-container>
@@ -36,30 +35,150 @@ export default {
   data() {
     return {
       cards: [
-        { id: 1, value: 1, flipped: false },
-        { id: 2, value: 1, flipped: false },
-        { id: 3, value: 2, flipped: false },
-        { id: 4, value: 2, flipped: false },
-        { id: 5, value: 3, flipped: false },
-        { id: 6, value: 3, flipped: false },
-        { id: 7, value: 4, flipped: false },
-        { id: 8, value: 4, flipped: false },
-        { id: 9, value: 5, flipped: false },
-        { id: 10, value: 5, flipped: false },
-        { id: 11, value: 6, flipped: false },
-        { id: 12, value: 6, flipped: false },
-        { id: 13, value: 7, flipped: false },
-        { id: 14, value: 7, flipped: false },
-        { id: 15, value: 8, flipped: false },
-        { id: 16, value: 8, flipped: false },
-        { id: 17, value: 9, flipped: false },
-        { id: 18, value: 9, flipped: false },
-        { id: 19, value: 10, flipped: false },
-        { id: 20, value: 10, flipped: false },
-        { id: 21, value: 11, flipped: false },
-        { id: 22, value: 11, flipped: false },
-        { id: 23, value: 12, flipped: false },
-        { id: 24, value: 12, flipped: false },
+        {
+          id: 1,
+          value: 1,
+          flipped: false,
+          src: "https://picsum.photos/id/1/120",
+        },
+        {
+          id: 2,
+          value: 1,
+          flipped: false,
+          src: "https://picsum.photos/id/1/120",
+        },
+        {
+          id: 3,
+          value: 2,
+          flipped: false,
+          src: "https://picsum.photos/id/2/120",
+        },
+        {
+          id: 4,
+          value: 2,
+          flipped: false,
+          src: "https://picsum.photos/id/2/120",
+        },
+        {
+          id: 5,
+          value: 3,
+          flipped: false,
+          src: "https://picsum.photos/id/3/120",
+        },
+        {
+          id: 6,
+          value: 3,
+          flipped: false,
+          src: "https://picsum.photos/id/3/120",
+        },
+        {
+          id: 7,
+          value: 4,
+          flipped: false,
+          src: "https://picsum.photos/id/4/120",
+        },
+        {
+          id: 8,
+          value: 4,
+          flipped: false,
+          src: "https://picsum.photos/id/4/120",
+        },
+        {
+          id: 9,
+          value: 5,
+          flipped: false,
+          src: "https://picsum.photos/id/5/120",
+        },
+        {
+          id: 10,
+          value: 5,
+          flipped: false,
+          src: "https://picsum.photos/id/5/120",
+        },
+        {
+          id: 11,
+          value: 6,
+          flipped: false,
+          src: "https://picsum.photos/id/6/120",
+        },
+        {
+          id: 12,
+          value: 6,
+          flipped: false,
+          src: "https://picsum.photos/id/6/120",
+        },
+        {
+          id: 13,
+          value: 7,
+          flipped: false,
+          src: "https://picsum.photos/id/7/120",
+        },
+        {
+          id: 14,
+          value: 7,
+          flipped: false,
+          src: "https://picsum.photos/id/7/120",
+        },
+        {
+          id: 15,
+          value: 8,
+          flipped: false,
+          src: "https://picsum.photos/id/8/120",
+        },
+        {
+          id: 16,
+          value: 8,
+          flipped: false,
+          src: "https://picsum.photos/id/8/120",
+        },
+        {
+          id: 17,
+          value: 9,
+          flipped: false,
+          src: "https://picsum.photos/id/9/120",
+        },
+        {
+          id: 18,
+          value: 9,
+          flipped: false,
+          src: "https://picsum.photos/id/9/120",
+        },
+        {
+          id: 19,
+          value: 10,
+          flipped: false,
+          src: "https://picsum.photos/id/10/120",
+        },
+        {
+          id: 20,
+          value: 10,
+          flipped: false,
+          src: "https://picsum.photos/id/10/120",
+        },
+        {
+          id: 21,
+          value: 11,
+          flipped: false,
+          src: "https://picsum.photos/id/11/120",
+        },
+        {
+          id: 22,
+          value: 11,
+          flipped: false,
+          src: "https://picsum.photos/id/11/120",
+        },
+        {
+          id: 23,
+          value: 12,
+          flipped: false,
+          src: "https://picsum.photos/id/12/120",
+        },
+        {
+          id: 24,
+          value: 12,
+          flipped: false,
+          src: "https://picsum.photos/id/12/120",
+        },
       ],
     };
   },
@@ -79,8 +198,10 @@ export default {
         return;
       } else if (flippedCards.length === 2) {
         if (flippedCards[0].value === flippedCards[1].value) {
-          alert("yay!");
-          this.cards = this.cards.filter((card) => !card.flipped);
+          setTimeout(
+            () => (this.cards = this.cards.filter((card) => !card.flipped)),
+            3000
+          );
         } else {
           setTimeout(() => {
             flippedCards[0].flipped = false;
